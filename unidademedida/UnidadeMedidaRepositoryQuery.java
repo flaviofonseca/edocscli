1ae7af9bc8ced252f04dc2eaab24c517633ed4a3
flaviofonse@gmail.com <flaviofonse@gmail.com>
2017-12-23 00:32:03 -0200

    Alteracoes na parte java

--- unidademedida/UnidadeMedidaRepositoryQuery.java
+++
@@ -1,6 +0,0 @@
-package br.com.edocs.app.unidademedida;
-
-import br.com.edocs.arquitetura.repository.GenericCustomRepository;
-
-public interface UnidadeMedidaRepositoryQuery extends GenericCustomRepository<UnidadeMedida> {
-}
